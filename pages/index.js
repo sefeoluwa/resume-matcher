a61b1543510252ccfdfdaefb84976e46b8ed7629
sefeoluwa <sefeoluwa01@gmail.com>
2023-09-04 21:29:01 +0100

    removed blog codes

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,5 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import {GraphQLClient, gql} from 'graphql-request'
-
-const graphcms = new GraphQLClient("https://api-ap-south-1.hygraph.com/v2/clm52dmlw34v501tfd8qmeljc/master")
-
-const QUERY = gql`
-ResumeMatchers {
-  resumeMatchers {
-    blogPosts
-    datePublished
-    id
-    slug
-    title
-    updatedAt
-    content{
-      html
-    }
-    author{
-      name,
-      avatar{
-        url
-      }
-    }
-    coverPhoto{
-      publishedAt{
-        createdBy{
-          id
-        }
-        url 
-      }
-    }
-  }
-}
-`;
 
 export default function Home() {
   return (
